996141599180f038a73cd3398566de458f8fbe51
carlaospa <carlaospa@gmail.com>
2021-11-08 21:45:13 -0300

    Update Project

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,6 +1,7 @@
 import { Transferencia } from './../models/transferencia.model';
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Output, EventEmitter } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
    selector:'app-nova-transferencia',
@@ -14,7 +15,7 @@ export class NovaTransferenciaComponent{
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService){
+  constructor(private service: TransferenciaService, private router: Router){
 
   }
 
@@ -28,6 +29,7 @@ export class NovaTransferenciaComponent{
       (resultado) => {
         console.log(resultado);
         this.limparCampos();
+        this.router.navigateByUrl('extrato');
       },
       (erro) => console.error(erro)
     );
